8a1bdb4644700f46527f9c7e468ad62114c72a59
jSUNSH1NEw <weertz.joffrey@protonmail.ch>
2023-08-03 10:59:40 +0000

    update Cleaning code

--- client/hooks/useMarketData.ts
+++ client/hooks/useMarketData.ts
@@ -8,9 +8,6 @@ const useMarketData = () => {
   const [liquidityHistoryByHours, setLiquidityHistoryByHours] = useState([]);
 
   useEffect(() => {
-
-        //ANCHOR Async call for  API V1 
-        //params fecth the tokenDayData
     const fetchDataV1 = async () => {
       try {
         const response = await fetch(
@@ -37,62 +34,71 @@ const useMarketData = () => {
           }
         );
         const data = await response.json();
-
+        //ANCHOR get data for data object in  graphQL response
         const SRGDatas = data.data.token.tokenDayData;
         const totalSupply = parseFloat(data.data.token.totalSupply);
 
-        //////////////////////////////////////////////////////::
-
-        const mapHourlyVolumeData = SRGDatas.flatMap((entry: any) => {
-          const date = new Date(parseInt(entry.date) * 1000);
-          const startOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-          const hours = Array.from({ length: 24 }, (_, i) => i);
-
-          return hours.map((hour) => {
-            const startOfHour = new Date(startOfDay.getTime() + hour * 60 * 60 * 1000);
-            const endOfHour = new Date(startOfDay.getTime() + (hour + 1) * 60 * 60 * 1000);
-
-            // Filter entries within the current hour
-            const entriesInHour = SRGDatas.filter((entry:any) => {
-              const entryDate = new Date(parseInt(entry.date) * 1000);
-              return entryDate >= startOfHour && entryDate < endOfHour;
-            });
-
-          // Calculate the sum of volume for the hour
-          const currentVolume = entriesInHour.reduce((sum:any, entry:any) => sum + parseFloat(entry.dailyVolumeUSD), 0);
-
-    return {
-      timestamp: startOfHour.toLocaleString(),
-      currentVolume,
-    };
-  });
-});
-        setLiquidityHistoryByHours(mapHourlyVolumeData)
-        //////////////////////////////////////////////////////::
-
+        //ANCHOR Map volume data
+        //TODO Call utils date algo
         const mapPriceData = SRGDatas.map((entry:any) => ({
           timestamp: new Date(parseInt(entry.date) * 1000).toLocaleString(),
           price: parseFloat(entry.dailyVolumeUSD) / totalSupply,
         }));
         setPriceHistory(mapPriceData);
 
-        const mapMarketData = SRGDatas.map((entry: any) => ({
+        //ANCHOR Map volume data
+        //TODO Call utils date algo
+        const mapVolumeData = SRGDatas.map((entry: any) => ({
           timestamp: new Date(parseInt(entry.date) * 1000).toLocaleString(),
           volume: parseFloat(entry.dailyVolumeUSD),
           liquidity: parseFloat(entry.totalLiquidityUSD),
         }));
-        setVolumeHistory(mapMarketData);
+        setVolumeHistory(mapVolumeData);
 
-        const mapliquidityHistory = SRGDatas.map((entry: any) => ({
+        //ANCHOR Map Liqudity data
+        //TODO Call utils date algo
+        const mapLiquidityHistory = SRGDatas.map((entry: any) => ({
           timestamp: new Date(parseInt(entry.date) * 1000).toLocaleString(),
           liquidity: parseFloat(entry.totalLiquidityUSD),
         }));
-        setLiquidityHistory(mapliquidityHistory);
+        setLiquidityHistory(mapLiquidityHistory);
+
+
+        //ANCHOR Map hoursly volume data
+        const mapHourlyVolumeData = SRGDatas.flatMap((entry: any) => {
+          //TODO Create a algo utils into api 
+          const date = new Date(parseInt(entry.date) * 1000);
+          //ANCHOR calculate by adding the number of milliseconds in an hour multiplied by the hour index
+          const startOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+          //ANCHOR calculates the start and end time of each hour in a given date.
+              const hours = Array.from({ length: 24 }, (_, i) => i);
+
+          return hours.map((hour) => {
+                // Adjust start of day with start and end from 0 to 23 of  hours/days (24)
+                const startOfHour = new Date(startOfDay.getTime() + hour * 60 * 60 * 1000);
+                const endOfHour = new Date(startOfDay.getTime() + (hour + 1) * 60 * 60 * 1000);
+
+                //ANCHOR Filter entries within the current hour
+                const entriesInHour = SRGDatas.filter((entry:any) => {
+                  const entryDate = new Date(parseInt(entry.date) * 1000);//Convert date Property to proper Dates 
+                  
+                  return entryDate >= startOfHour && entryDate < endOfHour; //supérieur ou égal à startOfHour et inférieur à endOfHour.
+                });
+
+              //ANCHOR Calculate the sum of volume for the hour
+              const currentVolume = entriesInHour.reduce((sum:any, entry:any) => sum + parseFloat(entry.dailyVolumeUSD), 0);
+
+                //ANCHOR return the sum of currentVolume
+              return {
+                timestamp: startOfHour.toLocaleString(),
+                currentVolume,
+              };
+          });
+        });
+        setLiquidityHistoryByHours(mapHourlyVolumeData)
       } catch (error) {
         console.error("Error fetching market data:", error);
       }
-
-      
     };
 
     fetchDataV1();
